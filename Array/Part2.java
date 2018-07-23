9cb8f621d435f9b8bf033319380d29347a739ffc
huahang1 <huahangkidd@gmail.com>
2018-07-23 10:57:37 -0700

    udpate unique pathII solution

--- Array/Part2.java
+++ Array/Part2.java
@@ -101,9 +101,27 @@ public int[][] generateMatrix(int n) {
     }
 
 //62. Unique Paths
+public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+//63. Unique Paths II
 public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];
+        //if anything blocks, this whole column will be not accessable, so mark all elements 0
         for(int i = 0; i < m; i++){
             if(obstacleGrid[i][0] == 1){
                 while(i<m){
@@ -128,22 +146,6 @@ public int uniquePathsWithObstacles(int[][] obstacleGrid) {
             }
         }
         return dp[m-1][n-1];
-}
-
-//63. Unique Paths II
-public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    int width = obstacleGrid[0].length;
-    int[] dp = new int[width];
-    dp[0] = 1;
-    for (int[] row : obstacleGrid) {
-        for (int j = 0; j < width; j++) {
-            if (row[j] == 1)
-                dp[j] = 0;
-            else if (j > 0)
-                dp[j] += dp[j - 1];
-        }
-    }
-    return dp[width - 1];
 }
 
 //64. Minimum Path Sum
