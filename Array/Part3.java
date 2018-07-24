0f04b7e02b06e6b1fe9e365510d59735e0444cb0
huahang1 <huahangkidd@gmail.com>
2018-07-24 11:31:09 -0700

    update maximum product subarray solution

--- Array/Part3.java
+++ Array/Part3.java
@@ -31,6 +31,7 @@ public List<List<Integer>> generate(int numRows)
       row.set(j, row.get(j)+row.get(j+1));
     }
     //because arguments in method at java is passed by value instead of reference, so we need to create new space for each row
+    //here the row value will get effected because each time we pass argument, it copies the old value and pass it.
 		allrows.add(new ArrayList<Integer>(row));
 	}
 	return allrows;
@@ -86,14 +87,14 @@ public int maxProfit(int[] prices) {
 //123. Best Time to Buy and Sell Stock III
 public int maxProfit(int[] prices) {
        int hold1 = Integer.MIN_VALUE, hold2 = Integer.MIN_VALUE;
-       int release1 = 0, release2 = 0;
+       int sell1 = 0, sell2 = 0;
        for(int i:prices){                              // Assume we only have 0 money at first
-           release2 = Math.max(release2, hold2+i);     // The maximum if we've just sold 2nd stock so far.
-           hold2    = Math.max(hold2,    release1-i);  // The maximum if we've just buy  2nd stock so far.
-           release1 = Math.max(release1, hold1+i);     // The maximum if we've just sold 1nd stock so far.
-           hold1    = Math.max(hold1,    -i);          // The maximum if we've just buy  1st stock so far. 
+           sell2 = Math.max(sell2, hold2+i);     // The maximum if we've just sold 2nd stock so far.
+           hold2 = Math.max(hold2, sell1-i);  // The maximum if we've just buy  2nd stock so far.
+           sell1 = Math.max(sell1, hold1+i);     // The maximum if we've just sold 1nd stock so far.
+           hold1 = Math.max(hold1, 0-i);          // The maximum if we've just buy  1st stock so far. 
        }
-       return release2; ///Since release1 is initiated as 0, so release2 will always higher than release1.
+       return sell2; ///Since release1 is initiated as 0, so release2 will always higher than release1.
 }
 
 //126. Word Ladder II
@@ -187,7 +188,6 @@ public int longestConsecutive(int[] num) {
         if (!map.containsKey(n)) {
             int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
             int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
-            // sum: length of the sequence n is in
             int sum = left + right + 1;
             map.put(n, sum);
             
@@ -209,27 +209,15 @@ public int longestConsecutive(int[] num) {
 }
 
 //152. Maximum Product Subarray
-int maxProduct(int A[], int n) {
-    // store the result that is the max we have found so far
-    int r = A[0];
-
-    // imax/imin stores the max/min product of
-    // subarray that ends with the current number A[i]
-    for (int i = 1, imax = r, imin = r; i < n; i++) {
-        // multiplied by a negative makes big number smaller, small number bigger
-        // so we redefine the extremums by swapping them
-        if (A[i] < 0)
-            swap(imax, imin);
-
-        // max/min product for the current number is either the current number itself
-        // or the max/min by the previous number times the current one
-        imax = max(A[i], imax * A[i]);
-        imin = min(A[i], imin * A[i]);
-
-        // the newly computed max value is a candidate for our global result
-        r = max(r, imax);
+public int maxProduct(int[] nums) {
+    int imin = 1, imax = 1, res = nums[0];
+    for(int i : nums){
+        if(i<=0){int tmp = imin; imin = imax;imax = tmp;}
+        imax = Math.max(i,imax*i);
+        imin = Math.min(i,imin*i);
+        res = Math.max(res,imax);
     }
-    return r;
+    return res;
 }
 
 //153. Find Minimum in Rotated Sorted Array
