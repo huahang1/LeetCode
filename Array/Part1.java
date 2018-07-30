0d2cbefd1d409f71d28624614a43361e62893c57
huahang1 <huahangkidd@gmail.com>
2018-07-30 12:41:28 -0700

    update solution for jump game II

--- Array/Part1.java
+++ Array/Part1.java
@@ -429,19 +429,18 @@ public int trap(int[] A){
 }
 
 //45. Jump Game II
-int jump(int A[], int n) {
-  if(n<2)return 0;
-  int level=0,currentMax=0,i=0,nextMax=0;
-
-  while(currentMax-i+1>0){		//nodes count of current level>0
-    level++;
-    for(;i<=currentMax;i++){	//traverse current level , and update the max reach of next level
-     nextMax=max(nextMax,A[i]+i);
-     if(nextMax>=n-1)return level;   // if last element is in level+1,  then the min jump=level 
+public int jump(int[] nums) {
+    int max = 0, target = 0, index = 0;
+    for(int i = 0; i < nums.length-1;i++){
+        //farthest place it could reach
+        max = Math.max(max,nums[i]+i);
+        if(i == target){
+            index++;
+            //update the target, setting for reaching so it could achieve minimum step goal
+            target = max;
+        }
     }
-    currentMax=nextMax;
-  }
-  return 0;
+    return index; 
 }
 
 //48. Rotate Image
