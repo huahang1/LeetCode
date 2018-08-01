11a8f7f8be525ec9482c29c6d9914917fe5ae1a4
huahang1 <huahangkidd@gmail.com>
2018-07-31 18:11:59 -0700

    update solution

--- Array/Part7.java
+++ Array/Part7.java
@@ -133,21 +133,17 @@ public int numSubarrayBoundedMax(int[] A, int L, int R) {
 
 //825. Friends Of Appropriate Ages
 public int numFriendRequests(int[] ages) {
-        int res = 0;
-        int[] numInAge = new int[121], sumInAge = new int[121];
-        
-        for(int i : ages) 
-            numInAge[i] ++;
-        
-        for(int i = 1; i <= 120; ++i) 
-            sumInAge[i] = numInAge[i] + sumInAge[i - 1];
-        
-        for(int i = 15; i <= 120; ++i) {
-            if(numInAge[i] == 0) continue;
-            int count = sumInAge[i] - sumInAge[i / 2 + 7];
-            res += count * numInAge[i] - numInAge[i]; //people will not friend request themselves, so  - numInAge[i]
+    int[] freq = new int[121];
+    int requests = 0;
+    for(int age:ages){
+        freq[age]++;
+    }
+    for(int i = 15; i <= 120; i++){
+        for(int j = i/2 + 8; j<= i; j++){
+            requests += freq[j] * (freq[i] - (i==j ? 1 : 0));
         }
-        return res;
+    }
+    return requests;
 }
 
 //830. Positions of Large Groups
