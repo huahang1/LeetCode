3a852d8d2b1d98c1587a00793d5a547531736c78
huahang1 <huahangkidd@gmail.com>
2018-08-03 17:33:54 -0700

    add solutions for 10 and 32

--- DP/Part1.java
+++ DP/Part1.java
@@ -22,3 +22,52 @@ private int dp(String s, int l, int r){
     }
     return r-1-l;
 }
+
+//10. Regular Expression Matching
+public boolean isMatch(String s, String p) {
+    if(s == null || p == null) return false;
+    boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+    dp[0][0] = true;
+    for(int j = 1; j < dp[0].length;j++){
+        if(p.charAt(j-1) == '*'){
+            if(dp[0][j-1] || j > 0 && dp[0][j-2]){
+                dp[0][j] = true;
+            }
+        }
+    }
+    for(int i = 1; i < dp.length;i++){
+        for(int j = 1; j < dp[0].length;j++){
+            if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.'){
+                dp[i][j] = dp[i-1][j-1];
+            }
+            if(p.charAt(j-1) == '*'){
+                if(s.charAt(i-1) != p.charAt(j-2) && p.charAt(j-2) != '.'){
+                    dp[i][j] = dp[i][j-2];
+                }else{
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1] || dp[i][j-2];
+                }
+            }
+        }
+    }
+    return dp[s.length()][p.length()];
+}
+
+//32. Longest Valid Parentheses
+public int longestValidParentheses(String s) {
+    int max = 0, open = 0;
+    char[] p = s.toCharArray();
+    int[] dp = new int[s.length()];
+    for(int i = 0; i < p.length;i++){
+        if(p[i] == '(') open++;
+        if(p[i] == ')' && open > 0){
+            dp[i] = 2 + dp[i-1];
+            if(i-dp[i] >= 0){
+                //here i-dp[i] is i-(dp[i-1]+2), add previous matched parenthese dp[i-(dp[i-1]+2)]
+                dp[i] += dp[i-dp[i]];
+            }
+            max = Math.max(max,dp[i]);
+            open--;
+        }
+    }
+    return max;
+}
