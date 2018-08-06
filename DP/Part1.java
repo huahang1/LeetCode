3f29939e71ef4e7053c102ad0098ee9aa311a2a4
huahang1 <huahangkidd@gmail.com>
2018-08-06 16:38:57 -0700

    solution added

--- DP/Part1.java
+++ DP/Part1.java
@@ -143,3 +143,66 @@ public int numDecodings(String s) {
     }
     return dp[n];
 }
+
+//96. Unique Binary Search Trees
+public int numTrees(int n) {
+    int[] dp = new int[n+1];
+    //empty tree
+    dp[0] = 1;
+    //root node
+    dp[1] = 1;
+    for(int i = 2; i <= n;i++){
+        for(int j = 1; j <= i;j++){
+            dp[i] += dp[j-1] * dp[i-j];
+        }
+    }
+    return dp[n];
+}
+
+//95. Unique Binary Search Trees II
+public List<TreeNode> generateTrees(int n) {
+    if(n == 0) return new LinkedList<>();
+    return generateSubTree(1,n);
+}
+private List<TreeNode> generateSubTree(int start, int end){
+    List<TreeNode> res = new LinkedList<>();
+    if(start > end){
+        //empty tree
+        res.add(null);
+        return res;
+    }
+    for(int i = start; i <= end; i++){
+        List<TreeNode> left = generateSubTree(start,i-1);
+        List<TreeNode> right = generateSubTree(i+1,end);
+        for(TreeNode l : left){
+            for(TreeNode r : right){
+                TreeNode root = new TreeNode(i);
+                root.left = l;
+                root.right = r;
+                res.add(root);
+            }
+        }
+    }
+    return res;
+}
+
+//97. Interleaving String
+public boolean isInterleave(String s1, String s2, String s3) {
+    int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
+    if(l1+l2 != l3) return false;
+    boolean[][] dp = new boolean[l1+1][l2+1];
+    for(int i = 0; i <= l1; i++){
+        for(int j = 0; j <= l2; j++){
+            if(i==0 && j==0){
+                dp[i][j] = true;
+            }else if(i == 0){
+                dp[i][j] = (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1));
+            }else if(j == 0){
+                dp[i][j] = (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+            }else{
+                dp[i][j] = (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1)) || (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1));
+            }
+        }
+    }
+    return dp[l1][l2];
+}
