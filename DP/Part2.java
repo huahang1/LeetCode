107df76c5af3914872c05926367f17e2212f20b9
huahang1 <huahangkidd@gmail.com>
2018-09-19 18:21:55 -0700

    q.376 added

--- DP/Part2.java
+++ DP/Part2.java
@@ -402,3 +402,25 @@ private int helper(int[][] dp, int s, int e){
     dp[s][e] = res;
     return res;
 }
+
+//376. Wiggle Subsequence
+public int wiggleMaxLength(int[] nums) {
+    if(nums.length == 0) return 0;
+    int[] down = new int[nums.length];
+    int[] up = new int[nums.length];
+    down[0] = 1;
+    up[0] = 1;
+    for(int i = 1; i < nums.length;i++){
+        if(nums[i] > nums[i-1]){
+            up[i] = down[i-1]+1;
+            down[i] = down[i-1];
+        }else if(nums[i] < nums[i-1]){
+            down[i] = up[i-1]+1;
+            up[i] = up[i-1];
+        }else{
+            up[i] = up[i-1];
+            down[i] = down[i-1];
+        }
+    }
+    return Math.max(up[nums.length-1],down[nums.length-1]);
+}
