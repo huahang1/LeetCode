83c1852e2f9606f28d20cb825fd05d334c1c9e04
huahang1 <huahangkidd@gmail.com>
2018-10-15 16:00:01 -0700

    q.87 added

--- DP/Part1.java
+++ DP/Part1.java
@@ -239,6 +239,31 @@ public int maximalRectangle(char[][] matrix) {
        return res;
 }
 
+//87. Scramble String
+public boolean isScramble(String s1, String s2) {
+        if(s1.equals(s2)) return true;
+        if(s1.length() != s2.length()) return false;
+        int[] count = new int[26];
+        for(int i = 0; i < s1.length(); i++){
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        for(int i = 1; i < s1.length(); i++){
+            if(isScramble(s1.substring(0,i),s2.substring(0,i)) && isScramble(s1.substring(i),s2.substring(i))){
+              return true;  
+            } 
+            if(isScramble(s1.substring(0,i),s2.substring(s1.length()-i)) && isScramble(s1.substring(i),s2.substring(0,s1.length()-i))){
+              return true;  
+            } 
+        }
+        return false;
+}
+
 //91. Decode Ways
 public int numDecodings(String s) {
     int n = s.length();
