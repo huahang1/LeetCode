0c539784c0a200619bdd946bcbfef18ef9e596c8
huahang1 <huahangkidd@gmail.com>
2018-11-02 15:17:53 -0700

    update solution 646

--- DP/Part4.java
+++ DP/Part4.java
@@ -180,7 +180,7 @@ public int findLongestChain(int[][] pairs) {
         Arrays.fill(dp,1);
         for(int i = 0; i < pairs.length; i++){
             for(int j = 0; j < i; j++){
-                dp[i] = Math.max(dp[i],pairs[i][0] > pairs[j][1] ? dp[j]+1 : dp[j]);
+                dp[i] = Math.max(dp[i],pairs[j][1] < pairs[i][0] ? dp[j]+1 : dp[j]);
             }
         }
         return dp[pairs.length-1];
